35a25ce6ced72b0438adb52876d751fe04f4aff1
veveronika <vera.suleymanova.05@bk.ru>
2025-04-17 21:46:35 +0300

    Задание по HTML

--- src/Events.js
+++ src/Events.js
@@ -1,36 +1,69 @@
-/*
-   1. Создайте функцию createButton(). Необходимо, чтобы эта функция осуществила вставку в body тег button с текстом: "Удали меня".
-      При клике по button удалить этот button.
-*/
-export function createButton() {}
+export function createButton() {
+  const button = document.createElement('button');
+  button.textContent = 'Удали меня';
+  button.addEventListener('click', function() {
+    this.remove();
+  });
+  document.body.appendChild(button);
+}
 
-/*
-   2. Создайте функцию createArrList(arr), в которую передается 1 параметр: arr - массив строк.
-      Функция выводит этот массив в виде маркированного списка внутри тега body.
-      При наведении курсора мыши на элемент списка у этого элемента создается атрибут title, в котором записан его текст.
-*/
-export function createArrList(arr) {}
+export function createArrList(arr) {
+  const ul = document.createElement('ul');
 
-/*
-   3. Создайте функцию createLink(), которая сгенерирует следующую разметку и вставит ее в body:
+  arr.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    li.addEventListener('mouseenter', function() {
+      this.setAttribute('title', this.textContent);
+    });
+    ul.appendChild(li);
+  });
 
-      <a href="https://tensor.ru/">tensor</a>
+  document.body.appendChild(ul);
+}
 
-      При первом клике по ссылке в конец ее текста через пробел дописывается ее href.
-      При следующем клике происходит действие по умолчанию (переход по ссылке в текущей вкладке).
-*/
-export function createLink() {}
+export function createLink() {
+  const link = document.createElement('a');
+  link.href = 'https://tensor.ru/';
+  link.textContent = 'tensor';
 
-/*
-   4. Создайте функцию createList(), которая сгенерирует следующую разметку и вставит ее в body:
+  link.addEventListener('click', function(e) {
+    if (!this.dataset.clicked) {
+      e.preventDefault();
+      this.textContent += ` ${this.href}`;
+      this.dataset.clicked = 'true';
+    }
 
-      <ul>
-         <li>Пункт</li>
-      </ul>
-      <button>Добавить пункт</button>
+  });
 
-      При клике по элементу li ему в конец текста добавляется восклицательный знак.
-      При клике по button в конец списка добавляется новый элемент li с текстом: "Пункт".
-      Клик по новому li также добавляет восклицательный знак в конец текста.
-*/
-export function createList() {}
+  document.body.appendChild(link);
+}
+
+export function createList() {
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  li.textContent = 'Пункт';
+  ul.appendChild(li);
+
+  const button = document.createElement('button');
+  button.textContent = 'Добавить пункт';
+
+  function handleLiClick() {
+    this.textContent += '!';
+  }
+
+  ul.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+      handleLiClick.call(e.target);
+    }
+  });
+
+  button.addEventListener('click', function() {
+    const newLi = document.createElement('li');
+    newLi.textContent = 'Пункт';
+    ul.appendChild(newLi);
+  });
+
+  document.body.appendChild(ul);
+  document.body.appendChild(button);
+}
